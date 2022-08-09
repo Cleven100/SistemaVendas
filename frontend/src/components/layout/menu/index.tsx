b35a56e67c60ada89b6f1595aadeddfd99e640a1
Cleven Nesello <83832324+Cleven100@users.noreply.github.com>
2022-08-09 12:57:35 -0300

    ComponentsMenu

--- frontend/src/components/layout/menu/index.tsx
+++ frontend/src/components/layout/menu/index.tsx
@@ -1,27 +1,39 @@
 import React from 'react'
+import Link from 'next/link';
 
 const Menu: React.FC = () => {
-  return (
-    
-    <aside className='column is-2 is-narrow-mobile is-fullheight section is-hidden-mobile'> 
-        <p className="menu-label is-hidden-touch">
-            My Sales
-        </p>
-         <ul className="menu-list">
-            <li>
-                <a href="#">
-                   <span className="icon"></span> Home
-                </a>
-                <a href="#">
-                   <span className="icon"></span> Register
-                </a>
-                <a href="#">
-                   <span className="icon"></span> Config
-                </a>
-            </li>
-         </ul>    
-    </aside>
-  )
+    return (
+
+        <aside className='column is-2 is-narrow-mobile is-fullheight section is-hidden-mobile'>
+            <p className="menu-label is-hidden-touch">
+                My Sales
+            </p>
+            <ul className="menu-list">
+                <MenuItem href="/" label="home" />
+                <MenuItem href="/signup" label="SignUp" />
+                <MenuItem href="/config" label="Config" />
+            </ul>
+        </aside>
+    )
 }
+
+interface MenuItemProps {
+    href: string;
+    label: string;
+}
+
+const MenuItem: React.FC<MenuItemProps> = (props: MenuItemProps) => {
+    return (
+        <li>
+            <Link href={props.href}>
+                <a>
+                    <span className="icon"></span> {props.label}
+                </a>
+            </Link>
+        </li>
+    )
+}
+
+
 
 export default Menu
